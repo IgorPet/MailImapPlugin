499ff9f3e16081881e7b4676b76d91bc1ec378b5
GitHub Action <action@github.com>
2020-09-30 13:39:58 +0000

    Template cleanup

--- src/main/kotlin/com/github/igorpet/mailimapplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/igorpet/mailimapplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.igorpet.mailimapplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.igorpet.mailimapplugin.MyBundle
 
 class MyApplicationService {
 
